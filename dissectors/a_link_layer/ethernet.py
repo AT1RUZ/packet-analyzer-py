9a5f5239374c93bc7d17971d1d5174a657dccf50
Alex-deLight2210 <128495644+Alex-deLight2210@users.noreply.github.com>
2025-04-07 23:34:41 -0400

    Implementado el metodo de validar el checksum para protocolo ipv4 y agregado dicho dato en el json

--- dissectors/a_link_layer/ethernet.py
+++ dissectors/a_link_layer/ethernet.py
@@ -24,45 +24,12 @@ class EthernetDissector(Dissector):
         next_dissector_id = ethertype
         packet.set_current_offset(14)
 
-        # if self.verificar_crc32_ethernet(packet):
+
         return packet.get_payload(), next_dissector_type, next_dissector_id, layer_data
-        # else:
-        #     return None, None, None, None
-
-        # El CRC es un metodo para verificar si la  subtrama perteneciente a la capa de enlace no esta corrupta o con datos perdidos
-        # para este protocolo se usa la funcion de verificacion crc32 de la biblioteca binascii
-        # La funcion debe recibir el puntero  y la informacion cruda del paquete
-
-    def verificar_crc32_ethernet(self, packet):
-        sizeFCSenBytes = 4 #los 4 ultimos bytes
-        dataLenght = packet.getrawDataSize() - sizeFCSenBytes # se le restan 32(valor de 4 bytes) a la longitud ya que los ultimos bytes no se consideran en el calculo
-        inicioFragmentoFCS =dataLenght
-        fcsDataFragment = packet.getRawData(inicioFragmentoFCS, sizeFCSenBytes)
-        datosParaElCalculo = packet.getRawData(0,dataLenght)
-        # binary_string = ''.join(format(byte, '08b') for byte in fcsDataFragment)
-        byteFCS = struct.unpack("<I", fcsDataFragment)[0]
-        calculated_crc = binascii.crc32(datosParaElCalculo,0x04C11DB7) & 0xFFFFFFFF  # Calcula crc sobre el fragmento sin los últimos 4 bytes (FCS) ni los primeros 8 bytes
-
-        return calculated_crc == byteFCS
 
 
-    # def verificar_crc32_ethernet(self, packet):
-    #     raw_data = packet.raw_data
-    #     sizeFCSenBytes = 4
-    #     longitud_sin_fcs = packet.getrawDataSize() - sizeFCSenBytes
-    #     inicio_fcs = packet.getrawDataSize() - sizeFCSenBytes
-    #     fcsBinario = packet.getRawData(inicio_fcs, sizeFCSenBytes)
-    #
-    #
-    #     # Calcular el CRC32 del fragmento del paquete sin el FCS
-    #     calculated_crc = binascii.crc32(raw_data[:longitud_sin_fcs],0xFFFFFFFF ) & 0x04C11DB7
-    #
-    #
-    #     # Convertir el fragmento FCS real (fcsBinario) a un entero para comparar
-    #     # Asumimos que el FCS está en formato little-endian (común en Ethernet)
-    #
-    #
-    #     return calculated_crc == fcsBinario
+
+
 
 
 
--- dissectors/b_network_layer/ipv4.py
+++ dissectors/b_network_layer/ipv4.py
@@ -1,6 +1,6 @@
 from dissectors.dissector import Dissector
 from dissectors.registry import *
-from utils.byte_ops import read_uint8
+from utils.byte_ops import read_uint8, read_uint16_be
 
 class IPv4Dissector(Dissector):
     def dissect(self, packet):
@@ -10,13 +10,23 @@ class IPv4Dissector(Dissector):
         
         if ihl < 20 or ihl > 60:
             raise ValueError(f"IHL inválido: {ihl} bytes")
-            
+
+        #Fragmento para verificar el correcto valor de suma de chequeo del paquete
+        ipv4Header = payload[:ihl]
+        checksumRecibido = (payload[10] << 8) + payload[11]
+        headerSinChecksum = bytearray(ipv4Header)
+        headerSinChecksum[10:12] = b'\x00\x00'
+
+        checksumCorrecto = self.calcularSuma_ipv4(bytes(headerSinChecksum)) == checksumRecibido
+        if checksumCorrecto:
+            print("en talla")
         protocol = read_uint8(payload[9])
         layer_data = ('IPv4', {
             'src_ip': '.'.join(map(str, payload[12:16])),
             'dst_ip': '.'.join(map(str, payload[16:20])),
             'protocol': protocol,
-            'header_length': ihl
+            'header_length': ihl,
+            'Checksum valido':checksumCorrecto
         })
         
         next_dissector_type =  'ip_proto_types'
@@ -25,5 +35,23 @@ class IPv4Dissector(Dissector):
         # Actualizar el offset para el siguiente protocolo
         packet.set_current_offset(packet.get_current_offset() + ihl)
         
-        return packet.get_payload(), next_dissector_type, next_dissector_id, layer_data 
+        return packet.get_payload(), next_dissector_type, next_dissector_id, layer_data
 
+    def calcularSuma_ipv4(self,header):
+        """Calculates the IPv4 header checksum."""
+        checksum = 0
+        # Iterate through 16-bit words in the header
+        for i in range(0, len(header), 2):
+            if i + 1 < len(header):
+                word = (header[i] << 8) + header[i + 1]
+            else:
+                word = (header[i] << 8)
+            checksum += word
+
+        # Handle overflow
+        while (checksum >> 16) > 0:
+            checksum = (checksum & 0xFFFF) + (checksum >> 16)
+
+        # One's complement
+        checksum = ~checksum & 0xFFFF
+        return checksum
